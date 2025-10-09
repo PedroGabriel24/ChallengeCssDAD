56cf3859c72ae0ce3a2ad495ee111b82194c846c
Pedro Gabriel <pedro.correia@germinare.org.br>
2025-10-09 11:10:55 -0300

    Finalizando as Partes do Projeto

--- challengecss/src/App.jsx
+++ challengecss/src/App.jsx
@@ -2,6 +2,9 @@ import './App.css'
 import MyComponent from './components/MyComponent'
 
 function App() {
+  const n = 15;
+  const redTitle = true;
+
   return (
     <>
       <div className='app'>
@@ -9,6 +12,30 @@ function App() {
 
         <MyComponent/>
         <p>Este parágrafo pegou o estilo do componente!</p>
+
+        <p
+            style={{
+                color: "blue",
+                padding: "25px",
+                borderTop: "1px dotted blue",
+              }}
+            >
+              Este elemento foi estilizado inline
+          </p>
+
+          <h2 style={n > 10 ? { color: "purple" } : { color: "magenta" }}>
+            CSS dinâmico
+          </h2>
+          <h2 style={n < 10 ? { color: "purple" } : { color: "magenta" }}>
+            CSS dinâmico 2
+          </h2>
+
+          <h2 className={redTitle ? "red-title" : "title"}>
+            Este título vai ter uma classe dinâmica
+          </h2>
+
+          <Title />
+          <h1 className="title">Este não recebe título do CSS modules</h1>
       </div>
     </>
   )
